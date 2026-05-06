98e7519686ee900ce656bb89fe62924184c9bbfe
69SigmaMale69 <iftekharhpc@gmail.com>
2026-05-06 23:29:02 +0100

    Initial submission

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+
+
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        GameFrame gameFrame = new GameFrame();
+    }
+}
+
+
+
+/
+
+
+// This will be used to run the code only
+//Im going to set up the sturcture first of the game before i even do the logic
+// next things will be made is the actual frame of the game ( window)
