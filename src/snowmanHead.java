a18692c6bd85b47c6f7d8b5d433ceec8979de8db
69SigmaMale69 <iftekharhpc@gmail.com>
2026-05-07 15:05:40 +0100

    SnowmanHead

---
+++ src/snowmanHead.java
@@ -0,0 +1,10 @@
+
+class SnowmanHead extends Piece {
+
+
+    SnowmanHead(int row, int col) {
+        super(row, col, "images/head_blue.png");
+    }
+}
+
+// ill add the other heads task 1 only needs the blue one
