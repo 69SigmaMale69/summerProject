5adbee11462efd6df3d27e50462f107e7ac99d14
69SigmaMale69 <iftekharhpc@gmail.com>
2026-05-08 13:55:57 +0100

    snowmanhead update

--- src/snowmanHead.java
+++ src/snowmanHead.java
@@ -1,10 +1,26 @@
 
 class SnowmanHead extends Piece {
 
+    // task 3 has 3 different heads now so I had to add a colour
+    // before this it was just hardcoded to just blue
+    String colour;
 
-    SnowmanHead(int row, int col) {
-        super(row, col, "images/head_blue.png");
+
+    SnowmanHead(int row, int col, String colour) {
+        super(row, col, "images/head_" + colour + ".png");
+        this.colour = colour;
     }
+
+
+    // I kept the old constructor working so my level 1 setup still
+    // works without me having to go and change it
+    SnowmanHead(int row, int col) {
+        this(row, col, "blue");
+    }
+
+
+    String getColour() { return colour; }
 }
 
-// ill add the other heads task 1 only needs the blue one
+
+// task 3 update, red and yellow shoulld be added now
